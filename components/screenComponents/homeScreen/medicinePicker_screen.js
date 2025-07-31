a62539c5e86c2eee931e0a935a9eee941f9ec1eb
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-07-31 17:08:25 +0700

    Update medicinePicker_screen.js
    - Add working search bar for data
    - add flatlist that listing medicine based on data
    - add logic to detect if medicine has been adden, to cart preview

--- components/screenComponents/homeScreen/medicinePicker_screen.js
+++ components/screenComponents/homeScreen/medicinePicker_screen.js
@@ -12,7 +12,7 @@ import {
   TouchableOpacity,
   LayoutAnimation,
   UIManager,
-  Platform
+  Platform,
 } from "react-native";
 import Collapsible from "react-native-collapsible";
 import { FlatList } from "react-native-gesture-handler";
@@ -24,123 +24,61 @@ import { LinearGradient } from "expo-linear-gradient";
 
 import UseIcons from "../../middleware/tools/useIcons";
 
+// import data dummy
+import dataObat from "../../../dummyData/dataObat.json";
+
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 
-// Component for Accordion
-const AccordionItem = React.memo(({ title, initialCollapsed = true }) => {
-  const dummyArray = Array(10).fill(null).map((_, index) => ({ id: `${index}` }));
-  const [isCollapsed, setIsCollapsed] = useState(initialCollapsed);
-
-  const toggleCollapse = () => {
-    setIsCollapsed(!isCollapsed);
-  };
-
-  const renderObatItem = ({item}) => {
-    return (
-    <View style={styles.obatItem}>
-      <View>
-        <Text style={[styles.normalText, { fontSize: 20 }]}>Paracetamol</Text>
-        <View style={{ flexDirection: "row", alignItems: "center" }}>
-          <Text style={{ fontSize: 20, marginInline: 3.5 }}>•</Text>
-          <Text style={[styles.normalText, { fontSize: 16 }]}>Kapsul</Text>
-          <Text style={{ fontSize: 20, marginInline: 3.5 }}>•</Text>
-          <Text style={[styles.normalText, { fontSize: 16 }]}>
-            Antibiotik
-          </Text>
-        </View>
-      </View>
-
-      <Text style={[styles.normalText, { fontSize: 20, color: "#616161" }]}>
-        2
-      </Text>
-    </View>
-    );
-  };
-
-  return (
-    <View style={{ marginBlockStart: 4 }}>
-      <Pressable
-        onPress={toggleCollapse}
-        style={{
-          alignContent: "center",
-          flexDirection: "row",
-          justifyContent: "space-between",
-        }}
-      >
-        <Text style={[styles.normalText, { fontSize: 18 }]}>{title}</Text>
-        <UseIcons
-          name={isCollapsed ? "arrow-right" : "arrow-drop-down"}
-          set="MaterialIcons"
-          color="#616161"
-        />
-      </Pressable>
-
-      <Collapsible collapsed={isCollapsed}>
-        <View style={[styles.itemContent]}>
-          <FlatList
-            data={dummyArray} // Data Source
-            renderItem={renderObatItem}
-            keyExtractor={(item) => item.id}
-            // Properti untuk mengontrol performa FlatList
-            initialNumToRender={3} // Jumlah item awal yang dirender
-            showsVerticalScrollIndicator={true} // Sembunyikan scroll indicator jika tidak diinginkan
-            contentContainerStyle={{gap: 12}}
-          />
-        </View>
-      </Collapsible>
-    </View>
-  );
-});
-
-// Component for History List
-const HistoryList = React.memo(({item}) => (
-  <View style={[styles.detailData]}>
-    <View
-      style={{ flexDirection: "row", alignItems: "center", gap: 10 }}
-    >
-      <UseIcons
-        name="calendar-alt"
-        set="FontAwesome5"
-        size={15}
-        color="#4ACDD1"
-      />
-
-      <View>
-        <Text style={[styles.normalText, { fontSize: 18 }]}>
-          {item.id}
+// Component for Medicine Entry Item
+const MedicineEntryItem = ({ item, action }) => (
+  <View style={[styles.medicineEntry]}>
+    <View style={{ flex: 1 }}>
+      <Text style={[styles.medText, { fontSize: 18 }]}>{item.namaObat}</Text>
+      <View style={[{ flexDirection: "row", alignItems: "center" }]}>
+        <Text style={[styles.normalText, { fontSize: 16 }]}>
+          {item.bentukFisikObat}
+        </Text>
+        <Text style={{ fontSize: 20, marginInline: 3.5 }}>•</Text>
+        <Text style={[styles.normalText, { fontSize: 16 }]}>
+          {item.jenisKhasiatObat}
         </Text>
       </View>
     </View>
-
-    <View style={{ gap: 8 }}>
-      <Text style={[styles.normalText, { fontSize: 18 }]}>
-        Pemeriksa :<Text style={[styles.medText]}> Dr. Keqing</Text>
-      </Text>
-      <Text style={[styles.normalText, { fontSize: 18 }]}>
-        Keluhan :<Text style={[styles.medText]}> Demam, Pilek</Text>
-      </Text>
-      <Text style={[styles.normalText, { fontSize: 18 }]}>
-        Diagnosa :<Text style={[styles.medText]}> TBC</Text>
+    <View style={{ flex: 1, alignItems: "center" }}>
+      <Text style={[styles.normalText, { fontSize: 16 }]}>Tersedia</Text>
+      <Text style={[styles.medText, { fontSize: 18 }]}>
+        {item.jumlahObat} PCs
       </Text>
     </View>
-
-    <AccordionItem
-      title="Daftar Resep Obat"
-    />
+    <Pressable
+      onPress={() => {
+        alert(JSON.stringify(item));
+        action(true);
+      }}
+    >
+      <UseIcons name="add-button" set="Custom" size={20} stroke="#4ACDD1" />
+    </Pressable>
   </View>
-));
+);
 
 // Default export
 export default function MedicinePicker({ route }) {
-  const dummyArray = Array(10).fill(null).map((_, index) => ({ id: `${index}` }));
   const navigation = useNavigation();
 
-  const renderDataHistory = ({item}) => {
-    return (
-      <HistoryList item={item}/>
-    );
+  const [searchQuery, setSearchQuery] = useState("");
+  const filteredDataObat = dataObat.filter(
+    (obat) =>
+      obat.namaObat.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      obat.bentukFisikObat.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      obat.jenisKhasiatObat.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  const [isMedicineHasBeenChoosen, setIsMedicineHasBeenChoosen] = useState(false);
+
+  const renderMedicineEntry = ({ item }) => {
+    return <MedicineEntryItem item={item} action={setIsMedicineHasBeenChoosen}/>;
   };
 
   return (
@@ -149,20 +87,24 @@ export default function MedicinePicker({ route }) {
         <View style={[styles.upperContent]}>
           <View style={styles.headerContainer}>
             <Pressable
-              style={[styles.actionCircleButtonContainer, {transform: [{ rotateZ: '80deg' }],}]}
+              style={[
+                styles.actionCircleButtonContainer,
+                { transform: [{ rotateZ: "80deg" }] },
+              ]}
               onPress={() => navigation.goBack()}
             >
-              <LinearGradient colors={['gray', '#FFFFFF']}
-                              start={{ x: 0, y: 2 }}
-                              end={{ x: 1, y: 1 }}
-                              style={styles.actionCircleButton}
+              <LinearGradient
+                colors={["gray", "#FFFFFF"]}
+                start={{ x: 0, y: 2 }}
+                end={{ x: 1, y: 1 }}
+                style={styles.actionCircleButton}
               >
                 <UseIcons
                   name="angle-left"
                   set="FontAwesome6"
                   size={20}
                   color="#4ACDD1"
-                  style={[{transform: [{ rotateZ: '-80deg' }],}]}
+                  style={[{ transform: [{ rotateZ: "-80deg" }] }]}
                 />
               </LinearGradient>
             </Pressable>
@@ -173,14 +115,44 @@ export default function MedicinePicker({ route }) {
         </View>
 
         <View style={[styles.lowerContent]}>
+            <View style={[styles.medicineListWrapper, {flex: 1, gap: 20, paddingInline: 16 }]}>
+                <View style={[styles.searchBox]}>
+                    <UseIcons
+                        name="search"
+                        set="FontAwesome"
+                        size={20}
+                        color="#BBBBBB"
+                    />
+                    <TextInput
+                        placeholder="Cari berdasarkan nama obat"
+                        onChangeText={setSearchQuery}
+                        value={searchQuery}
+                        style={{ paddingInlineEnd: "10%" }}
+                    />
+                </View>
 
+                <FlatList
+                data={filteredDataObat}
+                renderItem={renderMedicineEntry}
+                keyExtractor={(item) => item.id}
+                showsVerticalScrollIndicator={true}
+                contentContainerStyle={{ gap: 8, paddingBlockEnd: 8, }}
+                />
+            </View>
+            <View style={[styles.medicineCartWrapper,{flexGrow: 0, paddingBlock: 16, paddingInline: 16, borderTopWidth: 1, borderTopColor: "#00000026",backgroundColor: "#FFFFFF"}]}>
+              {isMedicineHasBeenChoosen && 
+              <Text>Test</Text>}
+              <Text>Test</Text>
+              <Text>Test</Text>
+              <Text>Test</Text>
+            </View>
         </View>
 
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -188,7 +160,6 @@ const styles = StyleSheet.create({
     // justifyContent: "center",
     height: windowHeight,
     paddingTop: 19,
-    paddingInline: 16,
     backgroundColor: "#F7F9FC",
   },
   headerContainer: {
@@ -202,7 +173,7 @@ const styles = StyleSheet.create({
     overflow: "hidden",
 
     shadowColor: "#000",
-    elevation: 2
+    elevation: 2,
   },
   actionCircleButton: {
     flex: 1,
@@ -240,36 +211,32 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "space-between",
     marginBlockEnd: 38,
+    paddingInline: 16,
   },
   lowerContent: {
-    borderRadius: 18,
-
-    paddingBlock: 32,
-    paddingInline: 18,
-
-    backgroundColor: "#FFF",
+    flex: 1,
   },
-  detailData: {
-    flexDirection: "column",
-    gap: 20,
+  searchBox: {
+    height: 52,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 10,
+    paddingInline: 14.5,
 
-    paddingBlock: 32,
-
-    borderTopWidth: 1,
+    borderWidth: 1,
+    borderRadius: 12,
     borderColor: "#EDEDED",
+
+    backgroundColor: "#FFFFFF",
   },
-  itemContent: {
-    marginBlockStart: 32,
-    maxHeight: 200,
-  },
-  obatItem: {
-    paddingInlineStart: 10,
-    paddingInlineEnd: 12,
-    paddingBlock: 6,
+
+  medicineEntry: {
+    borderRadius: 14,
+    paddingInline: 24,
+    paddingBlock: 14,
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "space-between",
-    borderBottomWidth: 0.5,
-    borderBottomColor: "#78797A80",
+    backgroundColor: "#FFF",
   },
 });
