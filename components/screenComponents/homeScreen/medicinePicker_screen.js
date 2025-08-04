41f89de893d4550280f1a0320904a56cafc44772
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-04 11:46:22 +0700

    Update medicinePicker_screen.js
    - Working modal

--- components/screenComponents/homeScreen/medicinePicker_screen.js
+++ components/screenComponents/homeScreen/medicinePicker_screen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 import {
   StyleSheet,
   Text,
@@ -16,6 +16,8 @@ import {
 } from "react-native";
 import Collapsible from "react-native-collapsible";
 import { FlatList } from "react-native-gesture-handler";
+import Modal from "react-native-modal";
+import ExtraDimensions from 'react-native-extra-dimensions-android';
 
 import { useNavigation } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -26,10 +28,13 @@ import UseIcons from "../../middleware/tools/useIcons";
 
 // import data dummy
 import dataObat from "../../../dummyData/dataObat.json";
+import { Use } from "react-native-svg";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
+const windowWidthED = ExtraDimensions.getRealWindowWidth();
+const windowHeightED = ExtraDimensions.getRealWindowHeight();
 
 // Component for Medicine Entry Item
 const MedicineEntryItem = ({ item, action }) => (
@@ -63,10 +68,59 @@ const MedicineEntryItem = ({ item, action }) => (
   </View>
 );
 
+// Component for Confirmation Modal
+const ComfirmationModal = ({isVisible, setVisibility, actionYes}) => (
+    <Modal
+      isVisible={isVisible}
+      statusBarTranslucent={true}
+      onBackdropPress={() => {setVisibility(false)}}
+      onBackButtonPress={() => {setVisibility(false)}}
+      animationIn ="tada"
+      animationInTiming={600}
+      animationOut ="fadeOutDown"
+      animationOutTiming={500}
+      backdropOpacity={0.3}
+    >
+      <View style={[styles.modalContentContainer, {alignItems:"center", justifyContent:"center"}]}>
+        <View style={[styles.modalContentWrapper, {paddingInline: 20, paddingBlock: 31, borderRadius: 16,backgroundColor: "#FFF"}]}> 
+          <View style={[styles.modalContent,{alignItems:"center", justifyContent:"center", gap: 20}]}>
+            <UseIcons
+              name="check-circle-fill"
+              set="Octicons"
+              size={40}
+              color="#ACE8EA"
+            />
+            <View style={{alignItems: "center", gap: 8}}>
+              <Text style={[styles.medText, {fontSize: 16,}]}>Konfirmasi Pemberian Obat?</Text>
+              <Text style={[styles.normalText, {fontSize: 14, textAlign: "center", lineHeight: 14.4}]}>Pastikan obat yang diberikan sudah sesuai sebelum melanjutkan</Text>
+            </View>
+
+            <View style={{flexDirection: "row", gap: 10}}>
+              <Pressable
+                style={[styles.button, styles.negativeButton]}
+                onPress={() => {setVisibility(false)}}
+              >
+                <Text style={[styles.medText,{fontSize: 12, color:"#4ACDD1"}]}>Batal</Text>
+              </Pressable>
+              <Pressable style={[styles.button, styles.positiveButton]}>
+                <Text style={[styles.medText,{fontSize: 12, color:"#FFF"}]}>Konfirmasi</Text>
+              </Pressable>
+            </View>
+          </View>
+        </View>
+      </View>
+    </Modal>
+);
+
 // Default export
 export default function MedicinePicker({ route }) {
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
+  console.log("Window Width: ", windowWidth, ",Window Height: ", windowHeight, " from Dimension");
+  console.log("Window Width: ", windowWidthED, ",Window Height: ", windowHeightED, " from ExtraDimension");
 
+
+  const [isModalVisible, setModalVisible] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const filteredDataObat = dataObat.filter(
     (obat) =>
@@ -75,15 +129,20 @@ export default function MedicinePicker({ route }) {
       obat.jenisKhasiatObat.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
-  const [isMedicineHasBeenChoosen, setIsMedicineHasBeenChoosen] = useState(false);
+  const [isMedicineHasBeenChoosen, setIsMedicineHasBeenChoosen] =
+    useState(false);
 
   const renderMedicineEntry = ({ item }) => {
-    return <MedicineEntryItem item={item} action={setIsMedicineHasBeenChoosen}/>;
+    return (
+      <MedicineEntryItem item={item} action={setIsMedicineHasBeenChoosen} />
+    );
   };
 
   return (
     <SafeAreaView style={{ backgroundColor: "#F7F9FC" }}>
-      <View style={styles.container}>
+    
+      <ComfirmationModal isVisible={isModalVisible} setVisibility={setModalVisible}/>
+      <View style={[styles.container]}>
         <View style={[styles.upperContent]}>
           <View style={styles.headerContainer}>
             <Pressable
@@ -99,7 +158,7 @@ export default function MedicinePicker({ route }) {
                 end={{ x: 1, y: 1 }}
                 style={styles.actionCircleButton}
               >
-                <UseIcons
+                <UseIcons 
                   name="angle-left"
                   set="FontAwesome6"
                   size={20}
@@ -115,42 +174,52 @@ export default function MedicinePicker({ route }) {
         </View>
 
         <View style={[styles.lowerContent]}>
-            <View style={[styles.medicineListWrapper, {flex: 1, gap: 20, paddingInline: 16 }]}>
-                <View style={[styles.searchBox]}>
-                    <UseIcons
-                        name="search"
-                        set="FontAwesome"
-                        size={20}
-                        color="#BBBBBB"
-                    />
-                    <TextInput
-                        placeholder="Cari berdasarkan nama obat"
-                        onChangeText={setSearchQuery}
-                        value={searchQuery}
-                        style={{ paddingInlineEnd: "10%" }}
-                    />
-                </View>
+          <View
+            style={[
+              styles.medicineListWrapper,
+              { flex: 1, gap: 20, paddingInline: 16 },
+            ]}
+          >
+            <View style={[styles.searchBox]}>
+              <UseIcons
+                name="search"
+                set="FontAwesome"
+                size={20}
+                color="#BBBBBB"
+              />
+              <TextInput
+                placeholder="Cari berdasarkan nama obat"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+                style={{ paddingInlineEnd: "10%" }}
+              />
+            </View>
 
-                <FlatList
-                data={filteredDataObat}
-                renderItem={renderMedicineEntry}
-                keyExtractor={(item) => item.id}
-                showsVerticalScrollIndicator={true}
-                contentContainerStyle={{ gap: 8, paddingBlockEnd: 8, }}
-                />
-            </View>
-            <View style={[styles.medicineCartWrapper,{flexGrow: 0, paddingBlock: 16, paddingInline: 16, borderTopWidth: 1, borderTopColor: "#00000026",backgroundColor: "#FFFFFF"}]}>
-              {isMedicineHasBeenChoosen && 
-              <Text>Test</Text>}
-              <Text>Test</Text>
-              <Text>Test</Text>
-              <Text>Test</Text>
-            </View>
+            <FlatList
+              data={filteredDataObat}
+              renderItem={renderMedicineEntry}
+              keyExtractor={(item) => item.id}
+              showsVerticalScrollIndicator={true}
+              contentContainerStyle={{ gap: 8, paddingBlockEnd: 8 }}
+            />
+          </View>
+          <View style={[styles.medicineCartWrapper, {paddingBlockEnd: (insets.bottom + 45),}]}>
+            <Text style={[styles.normalText, {fontSize: 1}]}>Daftar Resep Obat</Text>
+            <Pressable 
+              style={[styles.saveButton]}
+              onPress={() => {setModalVisible(true)}}
+            >
+              <Text style={[styles.medText, { fontSize: 18, color: "#fff" }]}>
+                Simpan
+              </Text>
+            </Pressable>
+          </View>
         </View>
 
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
+
   );
 }
 
@@ -229,7 +298,6 @@ const styles = StyleSheet.create({
 
     backgroundColor: "#FFFFFF",
   },
-
   medicineEntry: {
     borderRadius: 14,
     paddingInline: 24,
@@ -238,5 +306,36 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     backgroundColor: "#FFF",
+  },
+  medicineCartWrapper: {
+    gap: 20,
+    flexGrow: 0,
+    paddingBlock: 16,
+    paddingInline: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#00000026",
+    backgroundColor: "#fff",
+  },
+  saveButton: {
+    paddingBlock: 14,
+    borderRadius: 14,
+    alignItems: "center",
+    backgroundColor: "#4ACDD1",
+  },
+  button: {
+    height: 28, 
+    paddingInline: 12, 
+    borderRadius: 8, 
+    justifyContent:"center",
+    alignItems: "center"
+  },
+  negativeButton: {
+    borderWidth: 1, 
+    borderColor: "#4ACDD1",
+  },
+  positiveButton: {
+    borderWidth: 1,
+    borderColor: "#4ACDD1", 
+    backgroundColor: "#4ACDD1"
   },
 });
