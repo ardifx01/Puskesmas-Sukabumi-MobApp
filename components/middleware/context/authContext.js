1c6c814afedf21775c8345ba25fb4e7aae2c9731
rYuuXHikaRi <ryuux.devcode@gmail.com>
2025-08-27 12:11:45 +0700

    Create authContext.js

---
+++ components/middleware/context/authContext.js
@@ -0,0 +1,118 @@
+import React, { createContext, useState, useContext, useEffect, use } from 'react';
+import axios from 'axios';
+import * as SecureStore from 'expo-secure-store';
+import { set } from 'react-hook-form';
+
+const TOKEN_KEY = 'userToken';
+const USER_DATA = 'userData';
+export const API_URL = 'http://192.168.18.106:8080/api';
+
+const AuthContext = createContext({});
+
+export const useAuth = () => {
+    return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+    const [authData, setAuthData] = useState({
+        userToken: null,
+        userData: null,
+        isSignedIn: false,
+    });                
+
+    useEffect(() => {
+        const interceptor = axios.interceptors.response.use(
+            response => response,
+            async err => {  
+                console.log("Error Interceptor Caught an error: ", err);
+                if(err.response?.status === 400) {
+                    alert('Bad Request !, Token Status: ' + err.response?.data?.tokenStatus + ', ' + err.response?.data?.message);
+                } else if (err.response?.status === 401) {
+                    if(err.response?.data?.isLoginAttempt) {
+                       alert('Login failed !, Reason: ' + err.response?.data?.message);
+                    } else {
+                        console.log("401 Error, Unauthorized !, Token Status: ", err.response?.data?.tokenStatus, " Message: ", err.response?.data?.message);
+                        alert("Token Status:" + err.response?.data?.tokenStatus + " Message: " + err.response?.data?.message);
+                        console.log('Deleting token ', await SecureStore.getItemAsync(TOKEN_KEY));
+                        await SecureStore.deleteItemAsync(TOKEN_KEY);
+                        console.log('Token deleted, Token: ', await SecureStore.getItemAsync(TOKEN_KEY));
+                        console.log('Deleting user data ', await SecureStore.getItemAsync(USER_DATA));
+                        await SecureStore.deleteItemAsync(USER_DATA);
+                        console.log('User Data deleted, User Data: ', await SecureStore.getItemAsync(USER_DATA));
+                        console.log("Removing Auth Header...");
+                        axios.defaults.headers.common['Authorization'] = `Bearer ${null}`;
+                        console.log("Auth Header removed, Auth Header: ", axios.defaults.headers.common['Authorization']);
+                        console.log('Clearing Auth Data...');
+                        setAuthData({
+                            userToken: token,
+                            userData: userData,
+                            isSignedIn: true,
+                        });
+                        console.log("Auth Data cleared, Auth Data: ", authData);
+                    };
+
+                } else if(err.response?.status === 409) {
+                    alert("Conflict !, " + err.response?.data?.message);
+                } else if(err.response?.status === 422) {
+                    alert("Unprocessable Content, Reason: " + err.response?.data?.message);
+                } 
+                return Promise.reject(err);
+            }
+        );
+
+        const loadAuthData = async () => {
+            const token = await SecureStore.getItemAsync(TOKEN_KEY);
+            const userData = await SecureStore.getItemAsync(USER_DATA);
+            if (token) {
+                console.log('Token found:', token);
+                axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                console.log("Checking token validity...");
+                try {
+                    const tokenStatus = await axios.post(
+                                            `${API_URL}/user/check`, { 
+                                                headers: {
+                                                    'Accept': 'application/json',
+                                                    'Content-Type': 'application/json',
+                                                }
+                                            }
+                                        );
+                    console.log("Token Status:", tokenStatus?.data?.tokenStatus, " Data: ", tokenStatus?.data?.data);
+                    console.log("Setting Auth Data...");
+                    setAuthData({
+                        userToken: token,
+                        userData: userData,
+                        isSignedIn: true,
+                    });
+                } catch (error) {
+                    console.log("There is an error, check alert on phone !");
+                }
+            }
+        };
+        loadAuthData();
+    }, []);
+
+    const auth = async (username, password) => {
+        try {
+            const response = await axios.post(`${API_URL}/auth`, { 'email':username, 'password':password });
+            await SecureStore.setItemAsync(TOKEN_KEY, response.data.token);
+            await SecureStore.setItemAsync(USER_DATA, JSON.stringify(response.data.user));
+            setAuthData({
+                userToken: await SecureStore.getItemAsync(TOKEN_KEY),
+                userData: await SecureStore.getItemAsync(USER_DATA),
+                isSignedIn: true,
+            });
+            axios.defaults.headers.common['Authorization'] = `Bearer ${await SecureStore.getItemAsync(TOKEN_KEY)}`;
+        } catch (error) {
+           console.log("There is an error, check alert on phone !"); 
+        }
+        
+    };
+
+    const value = {
+        onLogin: auth,
+        // onLogout: deauth,
+        authData,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
